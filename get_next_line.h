2dddd3588c111c9a1f5926364aab3476365b6799
Yusuf GOKOL <ygokol@e1r7p8.42.fr>
2017-04-20 19:40:34 +0200

    encore gnl.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: ygokol <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/12 17:57:08 by ygokol            #+#    #+#             */
-/*   Updated: 2017/02/12 17:57:10 by ygokol           ###   ########.fr       */
+/*   Updated: 2017/04/20 19:39:57 by ygokol           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "libft/libft.h"
+# include "libft.h"
 # include <fcntl.h>
 # define BUFF_SIZE 42
 
